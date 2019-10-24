c267d5814cedd0256e4027d7db42b30f7e38f545
enzohilzinger <en.hzg53@gmail.com>
2019-10-24 21:11:04 +0200

    fixed decouple debug var

--- servicedashboardbe/settings.py
+++ servicedashboardbe/settings.py
@@ -147,13 +147,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
 
-if not config('DEBUG'):
+if not DEBUG:
     import django_heroku
     django_heroku.settings(locals())
 
